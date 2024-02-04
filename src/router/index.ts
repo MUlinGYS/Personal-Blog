e7e96c270158f7a738a8d54eebf57972b9529e24
kaichen <137163026+MUlinGYS@users.noreply.github.com>
2024-02-04 17:22:49 +0800

    更新路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import index from '../views/index/index.vue'
+import login from '../views/login/login.vue'
 
 const routes = [
 // 定义你的路由路径，每一个对象一个路由，例如：
 {
 path: '/',
-component: index
-},
+component: login
+    },
+    {   
+        name: 'index',
+        path: '/index',
+        component: () => import('../views/index/index.vue')
+    },
+    {
+        name: 'main',
+        path: '/main',
+        component: () => import('../views/main/main.vue')
+    },
 ]
 
 const router = createRouter({
