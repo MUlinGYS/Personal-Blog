f878ed5f0c054df87f50f7224b8d6838d442fc63
kaichen <137163026+MUlinGYS@users.noreply.github.com>
2024-02-04 17:22:15 +0800

    header主页头部

---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="headerimg">
+		<Menu></Menu>
+		<div
+			style="
+				width: 100vw;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			"
+		>
+			<img
+				class="rotate"
+				src="../assets/img/avatar.jpeg"
+			/>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts" name="Header">
+	import Menu from '@/components/Menu.vue';
+</script>
+
+<style scoped>
+	.headerimg {
+		position: relative;
+		width: 100vw;
+		height: 500px;
+		overflow: hidden;
+		background-size: 100% 100%;
+		background-color: #a7a8bd;
+		background-image: url('../assets/gif/header.gif');
+	}
+
+	.rotate {
+		animation: rotation 5s infinite linear;
+		width: 400px;
+		height: 400px;
+		border-radius: 50%;
+	}
+	@keyframes rotation {
+		from {
+			transform: rotate(0deg);
+		}
+		to {
+			transform: rotate(360deg);
+		}
+	}
+</style>
