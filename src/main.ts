82ff5f443bc15a83aaf7f78f40a3790527ace18d
kaichen <137163026+MUlinGYS@users.noreply.github.com>
2024-02-04 17:22:30 +0800

    引入element

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-    import { createApp } from 'vue'
-    import router from './router'  // 引入路由实例
-    import App from './App.vue'
+import { createApp } from 'vue'
+import router from './router'  // 引入路由实例
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-    createApp(App)
-      .use(router)   // 使用路由实例
-      .mount('#app')
+createApp(App)
+  .use(router)   // 使用路由实例
+  .use(ElementPlus)  // 使用 Element Plus
+  .mount('#app')   // 应用挂载点
