f3d744f19995bcfb476ed7dfc9f290665bf234c6
kaichen <137163026+MUlinGYS@users.noreply.github.com>
2024-02-04 17:24:49 +0800

    @别名映射

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import { resolve, dirname } from 'path';    // 导入 resolve, dirname
+import { fileURLToPath } from 'url';        // 导入 fileURLToPath
+
+// 将文件URL转换为路径
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')  // 将 '@' 的别名设置为 'src/' 
+    }
+  }
 })
