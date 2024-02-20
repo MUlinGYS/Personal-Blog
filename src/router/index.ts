0996da6baf6c86541c7cf175ecbb0547abbb997f
kaichen <137163026+MUlinGYS@users.noreply.github.com>
2024-02-20 15:27:53 +0800

    增加子路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,20 +2,22 @@ import { createRouter, createWebHistory } from 'vue-router'
 import login from '../views/login/login.vue'
 
 const routes = [
-// 定义你的路由路径，每一个对象一个路由，例如：
-{
-path: '/',
-component: login
-    },
-    {   
-        name: 'index',
-        path: '/index',
-        component: () => import('../views/index/index.vue')
+    // 定义你的路由路径，每一个对象一个路由，例如：
+    {
+        path: '/',
+        component: login
     },
     {
-        name: 'main',
-        path: '/main',
-        component: () => import('../views/main/main.vue')
+        name: 'index',
+        path: '/index',
+        component: () => import('../views/index/index.vue'),
+        children: [
+            {
+                name: 'main',
+                path: '',
+                component: () => import('../views/main/main.vue')
+            }
+        ],
     },
 ]
 
