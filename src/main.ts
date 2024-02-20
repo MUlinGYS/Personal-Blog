46ce7407e4b98751d47815c17938be1e2798904d
kaichen <137163026+MUlinGYS@users.noreply.github.com>
2024-02-20 15:27:39 +0800

    注册 Element Plus 图标

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,15 @@ import router from './router'  // 引入路由实例
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-createApp(App)
+const app = createApp(App)
+// 注册 Element Plus 图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app
   .use(router)   // 使用路由实例
   .use(ElementPlus)  // 使用 Element Plus
   .mount('#app')   // 应用挂载点
