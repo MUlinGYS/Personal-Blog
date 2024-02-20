ddaa622c50dcd5d172888645740b32cbdbc87607
kaichen <137163026+MUlinGYS@users.noreply.github.com>
2024-02-20 15:28:32 +0800

    添加页尾footer组件

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -2,18 +2,24 @@
 	<div class="common-layout">
 		<el-container>
 			<el-header><Header></Header></el-header>
-			<el-main> <router-view>Main</router-view></el-main>
-			<el-footer>Footer</el-footer>
+			<el-main> <router-view></router-view></el-main>
+			<el-footer><Footer></Footer></el-footer>
 		</el-container>
 	</div>
 </template>
 
 <script lang="ts" setup name="index">
 	import Header from '@/components/Header.vue';
+	import Footer from '../Footer/footer.vue';
 </script>
 
 <style scoped>
 	.el-header {
 		padding: 0 !important;
+		height: auto;
+	}
+	.el-footer {
+		padding: 0 !important;
+		height: auto;
 	}
 </style>
