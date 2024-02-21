d6d9ca6c0200bbb7c002c9eab46aeb3275dbf9cc
kaichen <137163026+MUlinGYS@users.noreply.github.com>
2024-02-21 17:12:00 +0800

    更新推荐位、增加接口

--- src/views/main/branch/AllTweets/tweets.vue
+++ src/views/main/branch/AllTweets/tweets.vue
@@ -1,6 +1,6 @@
 <template>
 	<!-- 推荐位 -->
-	<div>
+	<div style="padding: 2vw">
 		<el-carousel
 			height="400px"
 			direction="vertical"
@@ -8,24 +8,35 @@
 			:autoplay="true"
 		>
 			<el-carousel-item
-				v-for="item in 4"
-				:key="item"
+				v-for="image in imageInfo"
+				:key="image.id"
 			>
-				<h3
-					text="2xl"
-					justify="center"
-				>
-					{{ item }}
-				</h3>
+				<div style="position: relative; height: auto">
+					<p
+						style="
+							position: absolute;
+							top: 10px;
+							left: 10px;
+							background-color: transparent;
+							font-size: 10px;
+							color: white;
+							margin: 0;
+						"
+					>
+						{{ image.title }}
+					</p>
+					<img
+						:src="image.url"
+						style="height: auto"
+					/>
+				</div>
 			</el-carousel-item>
 		</el-carousel>
 	</div>
-	<div>
-		<el-empty description="tweets" />
-	</div>
+
 	<el-card shadow="hover">
 		<template #header>
-			<span>推文 name</span>
+			<span>推文name</span>
 		</template>
 		<div
 			v-for="o in 4"
@@ -34,12 +45,52 @@
 		>
 			{{ '内容 ' + o }}
 		</div>
-		<template #footer>其他content</template>
+		<template #footer>其他内容</template>
 	</el-card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import { ref, onMounted } from 'vue';
+	import axios from 'axios';
 
+	interface ImageInfo {
+		id: string;
+		url: string;
+		title: string;
+	}
+
+	// 初始化为可以存放图片信息的数组
+	const imageInfo = ref<ImageInfo[]>([]);
+
+	//获取图片信息
+	const fetchImage = async (): Promise<ImageInfo> => {
+		const response = await axios.get(
+			'https://api.vvhan.com/api/bing?size=1078x480&rand=sj&type=json'
+		);
+		if (response.data.success) {
+			return {
+				id: response.data.data.date,
+				url: response.data.data.url,
+				title: response.data.data.title,
+			};
+		} else {
+			console.error('Failed to fetch image:', response.data);
+			return {
+				id: '',
+				url: '',
+				title: '',
+			};
+		}
+	};
+
+	onMounted(async () => {
+		// 并发五次请求
+		const fetches = Array.from({ length: 5 }, () => fetchImage());
+		const images = await Promise.all(fetches);
+		// 将返回的图片信息赋值给imageInfo
+		imageInfo.value = images;
+	});
+</script>
 <style scoped>
 	.el-carousel__item h3 {
 		color: #475669;
