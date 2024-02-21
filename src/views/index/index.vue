f08ec2b316595670f3be6d00ea89b39f2e2ac5b1
kaichen <137163026+MUlinGYS@users.noreply.github.com>
2024-02-21 17:11:23 +0800

    更新娱乐工具位

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -2,7 +2,13 @@
 	<div class="common-layout">
 		<el-container>
 			<el-header><Header></Header></el-header>
-			<el-main> <router-view></router-view></el-main>
+			<el-main style="padding: 20px 1vw 0"
+				><div style="display: flex">
+					<Tool></Tool>
+					<router-view></router-view>
+					<Amusement></Amusement>
+				</div>
+			</el-main>
 			<el-divider border-style="dashed" />
 			<el-footer><Footer></Footer></el-footer>
 		</el-container>
@@ -12,6 +18,8 @@
 <script lang="ts" setup name="index">
 	import Header from '@/components/Header.vue';
 	import Footer from '../Footer/footer.vue';
+	import Tool from '../Amusement-ad/ad/tool.vue';
+	import Amusement from '../Amusement-ad/amusement/amusement.vue';
 </script>
 
 <style scoped>
