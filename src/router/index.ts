538d83cabf0bbe25bb4caa99a0056151762796f2
kaichen <137163026+MUlinGYS@users.noreply.github.com>
2024-02-28 16:19:03 +0800

    添加回到顶部按钮

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,6 +19,12 @@ const routes = [
             }
         ],
     },
+    {
+        name: 'publishpage',
+        path: '/publishpage',
+        component: () => import('../views/Publish page/PublishPage.vue'),
+    },
+    
 ]
 
 const router = createRouter({
